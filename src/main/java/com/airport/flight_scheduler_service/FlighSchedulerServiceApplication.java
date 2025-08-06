da185b2a0e1404630a9d3eb78499ca73ea5f50fc
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-06 16:51:34 +0200

    initial commit of flight scheduler service

---
+++ src/main/java/com/airport/flight_scheduler_service/FlighSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.airport.flight_scheduler_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighSchedulerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airport/flight_scheduler_service/FlighSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.flight_scheduler_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlighSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
