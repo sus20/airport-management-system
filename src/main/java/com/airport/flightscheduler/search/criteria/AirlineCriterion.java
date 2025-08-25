7cd151c82cbf0ef4bcdd5b9fa85310d6008a0249
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-25 22:58:18 +0200

    refactor(search): migrate AirlineCriterion to service.search.criteria and improve regex handling

---
+++ src/main/java/com/airport/flightscheduler/search/criteria/AirlineCriterion.java
@@ -0,0 +1,18 @@
+package com.airport.flightscheduler.search.criteria;
+
+import com.airport.flightscheduler.dto.FlightSearchRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class AirlineCriterion extends BaseCriterion {
+    @Override
+    public Optional<Criteria> toCriteria(FlightSearchRequest request) {
+        log.info("Airline: {}", request.getAirline());
+        return matchFieldWithCaseInsensitiveRegex("airline", request.getAirline());
+    }
+}
