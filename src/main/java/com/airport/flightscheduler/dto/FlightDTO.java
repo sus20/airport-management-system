e7c869063140cc4e752decf89a50edc6ae2caf59
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-25 23:13:28 +0200

    refactor(dto): hide id form FlightDTO JSON responses

--- src/main/java/com/airport/flightscheduler/dto/FlightDTO.java
+++ src/main/java/com/airport/flightscheduler/dto/FlightDTO.java
@@ -1,9 +1,9 @@
 package com.airport.flightscheduler.dto;
 
 import com.airport.flightscheduler.enumeration.FlightStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
@@ -11,12 +11,14 @@ import org.bson.types.ObjectId;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(NON_NULL)
 public class FlightDTO {
+    @JsonIgnore
     private ObjectId id;
     private String flightNumber;
     private String airline;
