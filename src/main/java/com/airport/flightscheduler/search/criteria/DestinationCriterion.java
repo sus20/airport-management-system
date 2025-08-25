8e8855b58b491bcaf9f242e6de629b6ab91981c8
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-25 23:06:56 +0200

    feat(search): add DestinationCriterion for filtering flights by destination

---
+++ src/main/java/com/airport/flightscheduler/search/criteria/DestinationCriterion.java
@@ -0,0 +1,15 @@
+package com.airport.flightscheduler.search.criteria;
+
+import com.airport.flightscheduler.dto.FlightSearchRequest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DestinationCriterion extends BaseCriterion {
+    @Override
+    public Optional<Criteria> toCriteria(FlightSearchRequest request) {
+        return matchFieldStrictUppercase("destination", request.getDestination());
+    }
+}
