7a3923b36f6b3539cc79df6d887127cc74252d22
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-09 22:52:56 +0200

    add initial setup for passenger check-in service with spring boot and maven wrapper

---
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/PassengerCheckInServiceApplication.java
@@ -0,0 +1,13 @@
+package com.airport.passenger_checkin_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerCheckInServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerCheckInServiceApplication.class, args);
+	}
+
+}
---
+++ passenger-checkin-service/src/test/java/com/airport/passenger_checkin_service/PassengerCheckInServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.passenger_checkin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerCheckInServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
