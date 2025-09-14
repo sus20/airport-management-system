df8eab054ea3fdc7958e1c8b34c585a5d9bada15
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-14 22:36:39 +0200

    chore: deleted unused import

--- flight-scheduler-service/src/main/java/com/airport/flightscheduler/controller/HomeController.java
+++ flight-scheduler-service/src/main/java/com/airport/flightscheduler/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.airport.flightscheduler.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
