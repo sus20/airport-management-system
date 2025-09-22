a3348cb8da435658fd16cd92b1e5da8b7108defb
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-22 20:25:01 +0200

    feat(service): add placeholder CheckInService interface

---
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/CheckInService.java
@@ -0,0 +1,12 @@
+package com.airport.passenger_checkin_service.service;
+
+import org.bson.types.ObjectId;
+
+public interface CheckInService {
+ /*   CheckInDTO checkIn(CheckInRequest request);
+    CheckInDTO getCheckIn(ObjectId id);
+    List<CheckInDTO> getCheckInsForFlight(ObjectId flightId);
+    CheckInDTO updateSeat(ObjectId checkInId, String seatNumber);
+    CheckInDTO updateBaggage(ObjectId checkInId, int baggageCount);
+    void cancelCheckIn(ObjectId checkInId);*/
+}
