632dfca069e17c3b3a779c86865e4d08cb67c35f
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-22 20:32:39 +0200

    config(mongo): enable Mongo auditing in main application class

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/PassengerCheckInServiceApplication.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/PassengerCheckInServiceApplication.java
@@ -2,12 +2,12 @@ package com.airport.passenger_checkin_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class PassengerCheckInServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PassengerCheckInServiceApplication.class, args);
 	}
-
 }
