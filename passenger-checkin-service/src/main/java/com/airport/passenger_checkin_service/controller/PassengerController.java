508cf4e3bf8bff15ef234e6bdbdcbdc5c0db92e7
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-22 23:26:10 +0200

    feat: extend PassengerController with updatePassenger (PUT) and enforce @Valid on requests

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/controller/PassengerController.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/controller/PassengerController.java
@@ -3,6 +3,7 @@ package com.airport.passenger_checkin_service.controller;
 import com.airport.passenger_checkin_service.dto.PassengerRequest;
 import com.airport.passenger_checkin_service.dto.PassengerResponse;
 import com.airport.passenger_checkin_service.service.PassengerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class PassengerController {
     private final PassengerService passengerService;
 
     @PostMapping
-    public ResponseEntity<PassengerResponse> createPassenger(@RequestBody PassengerRequest passengerRequest) {
+    public ResponseEntity<PassengerResponse> createPassenger(@Valid @RequestBody PassengerRequest passengerRequest) {
         return ResponseEntity.ok(passengerService.createPassenger(passengerRequest));
     }
 
@@ -29,5 +30,12 @@ public class PassengerController {
     @GetMapping("/{id}")
     public ResponseEntity<PassengerResponse> getPassengerById(@PathVariable ObjectId id) {
         return ResponseEntity.ok(passengerService.getPassengerById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PassengerResponse> updatePassenger(
+            @PathVariable ObjectId id,
+            @Valid @RequestBody PassengerRequest request) {
+        return ResponseEntity.ok(passengerService.updatePassenger(id, request));
     }
 }
