db06a26640b381ddabef4e92ca9aff565ebb67d0
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-23 13:35:35 +0200

    feat(dto): add CheckInRequest with validation for seats and baggage

---
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/dto/CheckInRequest.java
@@ -0,0 +1,26 @@
+package com.airport.passenger_checkin_service.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class CheckInRequest {
+    @NotBlank(message = "Flight ID is required")
+    private String flightId;
+    @NotBlank(message = "Passenger ID is required")
+    private String passengerId;
+
+    @NotEmpty(message = "At least one seat number must be provided")
+    @Size(max = 2, message = "A passenger cannot reserve more than 2 seats")
+    private Set<
+            @Pattern(
+                    regexp = "^[0-9]{1,2}[A-F]$",
+                    message = "Seat must be in format like 12A, 1B, 24F"
+            ) String> seatNumbers;
+
+    private boolean baggageChecked;
+    @Min(value = 0, message = "Baggage count cannot be negative")
+    private int baggageCount;
+}
