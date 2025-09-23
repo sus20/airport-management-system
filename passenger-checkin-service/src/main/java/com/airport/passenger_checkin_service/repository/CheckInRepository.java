bbf32b43add8865223743b23d64bae5077547c25
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-23 13:34:54 +0200

    feat: implement CheckInRepository extending MongoRepository with seat availability query

---
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/CheckInRepository.java
@@ -0,0 +1,9 @@
+package com.airport.passenger_checkin_service.repository;
+
+import com.airport.passenger_checkin_service.domain.CheckIn;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CheckInRepository extends MongoRepository<CheckIn, ObjectId> {
+    boolean existsByFlightIdAndSeatNumbersContains(String flightId, String seatNumber);
+}
