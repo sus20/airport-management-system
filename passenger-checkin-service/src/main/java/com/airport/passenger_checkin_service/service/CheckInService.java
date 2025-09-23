8f4e3472ae7eb42c8dbc7028019ae2ea51f2c889
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-23 13:37:52 +0200

    feat(service): activate CheckInService interface and start implementing checkIn.

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/CheckInService.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/CheckInService.java
@@ -1,12 +1,16 @@
 package com.airport.passenger_checkin_service.service;
 
+import com.airport.passenger_checkin_service.dto.CheckInRequest;
+import com.airport.passenger_checkin_service.dto.CheckInResponse;
 import org.bson.types.ObjectId;
 
+import java.util.List;
+
 public interface CheckInService {
- /*   CheckInDTO checkIn(CheckInRequest request);
-    CheckInDTO getCheckIn(ObjectId id);
-    List<CheckInDTO> getCheckInsForFlight(ObjectId flightId);
-    CheckInDTO updateSeat(ObjectId checkInId, String seatNumber);
-    CheckInDTO updateBaggage(ObjectId checkInId, int baggageCount);
-    void cancelCheckIn(ObjectId checkInId);*/
+    CheckInResponse checkIn(CheckInRequest request);
+    CheckInResponse getCheckIn(ObjectId id);
+    List<CheckInResponse> getCheckInsForFlight(ObjectId flightId);
+    CheckInResponse updateSeat(ObjectId checkInId, String seatNumber);
+    CheckInResponse updateBaggage(ObjectId checkInId, int baggageCount);
+    void cancelCheckIn(ObjectId checkInId);
 }
