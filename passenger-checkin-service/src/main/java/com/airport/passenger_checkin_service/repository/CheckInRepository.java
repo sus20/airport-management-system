cab4ae55f6ecf9897b1f3993afe1232037896b06
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-24 16:57:18 +0200

    refactor: update CheckInRepository to use FlightCheckInRecord entity, add findByFlightId query, and keep seat/passenger existence checks

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/CheckInRepository.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/CheckInRepository.java
@@ -1,10 +1,13 @@
 package com.airport.passenger_checkin_service.repository;
 
-import com.airport.passenger_checkin_service.domain.CheckIn;
+import com.airport.passenger_checkin_service.domain.FlightCheckInRecord;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CheckInRepository extends MongoRepository<CheckIn, ObjectId> {
+import java.util.List;
+
+public interface CheckInRepository extends MongoRepository<FlightCheckInRecord, ObjectId> {
+    List<FlightCheckInRecord> findByFlightId(String flightId);
     boolean existsByFlightIdAndPassengerId(String flightId, String passengerId);
     boolean existsByFlightIdAndSeatNumbersContains(String flightId, String seatNumber);
 }
