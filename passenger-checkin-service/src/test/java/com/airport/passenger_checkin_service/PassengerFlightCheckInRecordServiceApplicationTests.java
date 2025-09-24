4d19a99261330d48e645eb06289e0ea5ae2c0144
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-24 17:10:53 +0200

    refactor: update test class name to match FlightCheckInRecord naming convention

--- passenger-checkin-service/src/test/java/com/airport/passenger_checkin_service/PassengerFlightCheckInRecordServiceApplicationTests.java
+++ passenger-checkin-service/src/test/java/com/airport/passenger_checkin_service/PassengerFlightCheckInRecordServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PassengerCheckInServiceApplicationTests {
+class PassengerFlightCheckInRecordServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
