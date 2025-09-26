479f9c21fe78e3e1eaafbcfc9549d71274f6f7fc
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-26 11:05:31 +0200

    fix: add missing PassengerSearchRequest import in PassengerService

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/PassengerService.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.airport.passenger_checkin_service.service;
 
 import com.airport.passenger_checkin_service.dto.PassengerRequest;
 import com.airport.passenger_checkin_service.dto.PassengerResponse;
+import com.airport.passenger_checkin_service.dto.PassengerSearchRequest;
 import org.bson.types.ObjectId;
 
 import java.util.List;
@@ -12,4 +13,5 @@ public interface PassengerService {
     List<PassengerResponse> getAllPassengers();
     PassengerResponse updatePassenger(ObjectId id, PassengerRequest request);
     void deletePassenger(ObjectId id);
+    List<PassengerResponse> search(PassengerSearchRequest request);
 }
