d2d53a0c35035bf5762d333ddc2e12e43ae8c3c5
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-26 11:07:12 +0200

    Add passenger search functionality in PassengerServiceImpl

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/implementation/PassengerServiceImpl.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/implementation/PassengerServiceImpl.java
@@ -3,15 +3,19 @@ package com.airport.passenger_checkin_service.service.implementation;
 import com.airport.passenger_checkin_service.domain.Passenger;
 import com.airport.passenger_checkin_service.dto.PassengerRequest;
 import com.airport.passenger_checkin_service.dto.PassengerResponse;
+import com.airport.passenger_checkin_service.dto.PassengerSearchRequest;
 import com.airport.passenger_checkin_service.exception.DuplicatePassengerException;
 import com.airport.passenger_checkin_service.exception.PassengerNotFoundException;
 import com.airport.passenger_checkin_service.mapper.PassengerMapper;
 import com.airport.passenger_checkin_service.repository.PassengerRepository;
+import com.airport.passenger_checkin_service.search.SearchSpecification;
 import com.airport.passenger_checkin_service.service.PassengerService;
 import com.airport.passenger_checkin_service.service.validation.PassengerUniquenessRule;
 import com.mongodb.DuplicateKeyException;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,7 +27,8 @@ public class PassengerServiceImpl implements PassengerService {
     private final PassengerRepository passengerRepository;
     private final PassengerMapper passengerMapper;
     private final PassengerUniquenessRule passportRule;
-
+    private final SearchSpecification searchSpecification;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public PassengerResponse createPassenger(PassengerRequest request) {
@@ -64,6 +69,13 @@ public class PassengerServiceImpl implements PassengerService {
                     String.format("Passenger with id %s not found!", id));
         }
         passengerRepository.deleteById(id);
+    }
+
+    @Override
+    public List<PassengerResponse> search(PassengerSearchRequest request) {
+        Query query = searchSpecification.toQuery(request);
+        List<Passenger> passengers = mongoTemplate.find(query, Passenger.class);
+        return passengerMapper.toPassengerResponses(passengers);
     }
 
     private Passenger findPassengerById(ObjectId id) {
