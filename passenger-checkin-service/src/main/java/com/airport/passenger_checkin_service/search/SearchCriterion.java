933cb97c1d615dbc44e6d2767c96abb2b00abe09
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-26 10:25:37 +0200

    Add @Component annotation to criteria classes for Spring bean detection

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/search/SearchCriterion.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/search/SearchCriterion.java
@@ -1,10 +1,10 @@
 package com.airport.passenger_checkin_service.search;
 
-import com.airport.passenger_checkin_service.dto.PassengerRequest;
+import com.airport.passenger_checkin_service.dto.PassengerSearchRequest;
 import org.springframework.data.mongodb.core.query.Criteria;
 
 import java.util.Optional;
 
 public interface SearchCriterion {
-    Optional<Criteria> toCriteria( PassengerRequest request );
+    Optional<Criteria> toCriteria( PassengerSearchRequest request );
 }
--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/search/criteria/passenger/PhoneNumberCriterion.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/search/criteria/passenger/PhoneNumberCriterion.java
@@ -1,14 +1,16 @@
 package com.airport.passenger_checkin_service.search.criteria.passenger;
 
-import com.airport.passenger_checkin_service.dto.PassengerRequest;
+import com.airport.passenger_checkin_service.dto.PassengerSearchRequest;
 import com.airport.passenger_checkin_service.search.BaseCriterion;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class PhoneNumberCriterion extends BaseCriterion {
     @Override
-    public Optional<Criteria> toCriteria(PassengerRequest request) {
+    public Optional<Criteria> toCriteria(PassengerSearchRequest request) {
         return matchFieldWithCaseInsensitiveRegex("phoneNumber", request.getPhoneNumber());
     }
 }
