eedad2974c8713ae78842c946d8f1557ca092091
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-27 18:20:04 +0200

    refactor(exception): use flightNumber instead of flightId in SeatAlreadyTakenException

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/exception/SeatAlreadyTakenException.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/exception/SeatAlreadyTakenException.java
@@ -3,7 +3,7 @@ package com.airport.passenger_checkin_service.exception;
 import java.util.List;
 
 public class SeatAlreadyTakenException extends RuntimeException {
-    public SeatAlreadyTakenException(String flightId, List<String> seats) {
-        super("Seat(s) " + seats + " already taken for flight " + flightId);
+    public SeatAlreadyTakenException(String flightNumber, List<String> seats) {
+        super("Seat(s) " + seats + " already taken for flight " + flightNumber);
     }
 }
