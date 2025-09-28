bdeff5f4bee71ff93586e735f53338a1318189d9
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-28 11:32:01 +0200

    feat: map Flight entity to MongoDB collection - Added @Document annotation with collection "flights"

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/entity/Flight.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/entity/Flight.java
@@ -6,13 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "flights")
 public class Flight {
     @Id
     private ObjectId id;
+
     private String flightNumber;
     private FlightStatus status;
 }
