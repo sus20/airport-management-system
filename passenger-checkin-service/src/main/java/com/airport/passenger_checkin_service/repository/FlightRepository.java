7ea86f281cc7bfcda7b07a65766ba7dfed25eab0
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-28 19:39:15 +0200

    refactor: update FlightRepository to use FlightReference
    - Replaced Flight with FlightReference as the repository entity type
    - Updated findByFlightNumber() to return Optional<FlightReference>
    - Aligns repository with new lightweight flight reference model

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/FlightRepository.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/FlightRepository.java
@@ -1,11 +1,11 @@
 package com.airport.passenger_checkin_service.repository;
 
-import com.airport.passenger_checkin_service.domain.entity.Flight;
+import com.airport.passenger_checkin_service.domain.entity.FlightReference;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface FlightRepository extends MongoRepository<Flight, ObjectId> {
-    Optional<Flight> findByFlightNumber(String flightNumber);
+public interface FlightRepository extends MongoRepository<FlightReference, ObjectId> {
+    Optional<FlightReference> findByFlightNumber(String flightNumber);
 }
