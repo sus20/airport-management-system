620d7b6eb9778bac3d320371133b5366dc698aa1
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-28 11:39:00 +0200

    feat: add FlightEventConsumer to process flight events from Kafka
    - Consumes events form flights topic

---
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/consumer/FlightEventConsumer.java
@@ -0,0 +1,23 @@
+package com.airport.passenger_checkin_service.kafka.consumer;
+
+import com.airport.passenger_checkin_service.domain.event.FlightEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FlightEventConsumer {
+    private final FlightEventDispatcher dispatcher;
+
+    @KafkaListener(
+            topics = "${app.kafka.topic.flights}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consumeFlightEvent(FlightEvent event) {
+        log.info("Consuming flight event: {}", event);
+        dispatcher.dispatch(event);
+    }
+}
