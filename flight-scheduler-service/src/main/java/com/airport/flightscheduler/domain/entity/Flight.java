810331b952acd89d1e701250e03050ca72bae956
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-28 18:47:32 +0200

    refactor: replace FlightDTO with Flight entity

---
+++ flight-scheduler-service/src/main/java/com/airport/flightscheduler/domain/entity/Flight.java
@@ -0,0 +1,33 @@
+package com.airport.flightscheduler.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "flights")
+@CompoundIndexes({
+        @CompoundIndex(name = "unique_flight_schedule", def = "{'flightNumber': 1, 'departureTime': 1}", unique = true)
+})
+public class Flight {
+    @Id
+    private ObjectId id;
+    private String flightNumber;
+    private String airline;
+    private String origin;
+    private String destination;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private String aircraftType;
+    private BigDecimal price;
+}
