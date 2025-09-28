7704523b23befaf35219ae83869c80aae517ef48
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-28 19:23:26 +0200

    feat: add FlightEventPublisher for Kafka integration

---
+++ flight-scheduler-service/src/main/java/com/airport/flightscheduler/kafka/producer/FlightEventPublisher.java
@@ -0,0 +1,48 @@
+package com.airport.flightscheduler.kafka.producer;
+
+import com.airport.flightscheduler.domain.entity.Flight;
+import com.airport.flightscheduler.domain.event.FlightEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class FlightEventPublisher {
+    @Value("${app.kafka.topic.flights}")
+    private  String flightsTopic;
+
+    private final EventProducer<FlightEvent> eventProducer;
+
+    public  void publishFlightCreated(Flight flight){
+        FlightEvent event = FlightEvent.builder()
+                .eventType("CREATED")
+                .flight(flight)
+                .timestamp(Instant.now())
+                .build();
+        eventProducer.send(flightsTopic,flight.getFlightNumber(), event);
+    }
+
+    public void publishFlightUpdated(Flight flight) {
+        FlightEvent event = FlightEvent.builder()
+                .eventType("UPDATED")
+                .flight(flight)
+                .timestamp(Instant.now())
+                .build();
+
+        eventProducer.send(flightsTopic, flight.getFlightNumber(), event);
+    }
+
+    public void publishFlightDeleted(Flight flight) {
+        FlightEvent event = FlightEvent.builder()
+                .eventType("DELETED")
+                .flight(flight)
+                .timestamp(Instant.now())
+                .build();
+
+        eventProducer.send(flightsTopic, flight.getFlightNumber(), event);
+    }
+
+}
