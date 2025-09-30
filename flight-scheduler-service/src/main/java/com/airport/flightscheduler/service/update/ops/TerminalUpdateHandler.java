46aff5c33ca55ad2afb304317efa0353d31ed103
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-09-30 10:05:59 +0200

    Add TerminalUpdateHandler to handle terminal update events by applying terminal changes form FlightOpsRequest to Flight.

---
+++ flight-scheduler-service/src/main/java/com/airport/flightscheduler/service/update/ops/TerminalUpdateHandler.java
@@ -0,0 +1,28 @@
+package com.airport.flightscheduler.service.update.ops;
+
+import com.airport.flightscheduler.domain.dto.request.FlightOpsRequest;
+import com.airport.flightscheduler.domain.entity.Flight;
+import com.airport.flightscheduler.domain.enums.UpdateType;
+import com.airport.flightscheduler.service.update.FlightUpdateHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TerminalUpdateHandler implements FlightUpdateHandler<FlightOpsRequest> {
+    @Override
+    public Class<FlightOpsRequest> getSupportedType() {
+        return FlightOpsRequest.class;
+    }
+
+    @Override
+    public UpdateType getUpdateType() {
+        return UpdateType.TERMINAL_UPDATED;
+    }
+
+    @Override
+    public Flight applyUpdate(Flight flight, FlightOpsRequest request) {
+        if (request.getTerminal() != null && !request.getTerminal().equals(flight.getTerminal())) {
+            flight.setTerminal(request.getTerminal());
+        }
+        return flight;
+    }
+}
