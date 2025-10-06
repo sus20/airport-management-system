9750560681a415b52fe9b888c51ed1231c610dec
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-06 11:14:54 +0200

    Update CheckInStatus enum to include NOT_CHECKED_IN status

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/enums/CheckInStatus.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/enums/CheckInStatus.java
@@ -1,10 +1,9 @@
 package com.airport.passenger_checkin_service.domain.enums;
 
 public enum CheckInStatus {
-    BOOKED,       // Reservation confirmed
-    CHECKEDIN,    // Checked in, boarding pass issued
-    BOARDED,      // Passenger is onboard
-    FLOWN,        // Passenger has completed flight
-    NOSHOW,       // Passenger failed to show up
-    OFFLOADED     // Passenger was checked in but removed
+    NOT_CHECKED_IN,
+    CHECKED_IN,
+    BOARDING,
+    BOARDED,
+    NO_SHOW
 }
