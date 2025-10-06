a28ba1c5a2d6e82e113d27a7b74390ba521b5bdf
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-06 11:31:16 +0200

    chore: add AllArgsConstructor import in CheckInEventPublisher

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/producer/CheckInEventPublisher.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/producer/CheckInEventPublisher.java
@@ -4,10 +4,10 @@ import com.airport.passenger_checkin_service.domain.event.CheckInEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class CheckInEventPublisher {
     @Value("${app.kafka.topic.passenger-checkins}")
