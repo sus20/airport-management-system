d7c56eee7c206a3d6dc73f4edb03e98e7e194e27
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-06 11:26:48 +0200

    Update FlightEventHandler to use UpdateType enum

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/handler/FlightEventHandler.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/handler/FlightEventHandler.java
@@ -1,8 +1,10 @@
 package com.airport.passenger_checkin_service.kafka.handler;
 
+import com.airport.passenger_checkin_service.domain.enums.UpdateType;
 import com.airport.passenger_checkin_service.domain.event.FlightEvent;
 
 public interface FlightEventHandler {
+    String getEventType();
+    UpdateType getUpdateType();
     void handle(FlightEvent event);
-    String getType();
 }
