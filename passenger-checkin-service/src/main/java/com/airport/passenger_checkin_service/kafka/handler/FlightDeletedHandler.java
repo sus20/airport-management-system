fc45c7f8227487388163f0c2de3fa541976759b1
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-06 11:40:20 +0200

    Refactor FlightDeletedHandler to use FlightPayload and add logging

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/handler/FlightDeletedHandler.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/kafka/handler/FlightDeletedHandler.java
@@ -1,22 +1,34 @@
 package com.airport.passenger_checkin_service.kafka.handler;
 
+import com.airport.passenger_checkin_service.domain.enums.UpdateType;
 import com.airport.passenger_checkin_service.domain.event.FlightEvent;
 import com.airport.passenger_checkin_service.repository.FlightRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class FlightDeletedHandler implements FlightEventHandler{
+@Slf4j
+public class FlightDeletedHandler implements FlightEventHandler {
     private final FlightRepository flightRepository;
 
     @Override
-    public void handle(FlightEvent event) {
-        flightRepository.deleteById(event.getFlightReference().getId());
+    public String getEventType() {
+        return "DELETED";
     }
 
     @Override
-    public String getType() {
-        return "DELETED";
+    public UpdateType getUpdateType() {
+        return null;
+    }
+
+    @Override
+    public void handle(FlightEvent event) {
+        String flightId = event.getFlightPayload().getId();
+        flightRepository.deleteById(new ObjectId(flightId));
+        log.info("Deleted flight {} locally", event.getFlightPayload().getId());
+
     }
 }
