8906881656d24d06bf49b4e36e3939459086b33e
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-06 11:07:48 +0200

    update FlightRepository to use FlightDetails instead of FlightReference

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/FlightRepository.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/repository/FlightRepository.java
@@ -1,11 +1,11 @@
 package com.airport.passenger_checkin_service.repository;
 
-import com.airport.passenger_checkin_service.domain.entity.FlightReference;
+import com.airport.passenger_checkin_service.domain.entity.FlightDetails;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface FlightRepository extends MongoRepository<FlightReference, ObjectId> {
-    Optional<FlightReference> findByFlightNumber(String flightNumber);
+public interface FlightRepository extends MongoRepository<FlightDetails, ObjectId> {
+    Optional<FlightDetails> findByFlightNumber(String flightNumber);
 }
