e671357c7d08c15e6b7a434afccb9390153269ac
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-08 15:03:13 +0200

    refactor: simplify FlightRequestValidator using StringUtils and remove redundant null checks

--- flight-scheduler-service/src/main/java/com/airport/flightscheduler/domain/dto/request/validation/FlightRequestValidator.java
+++ flight-scheduler-service/src/main/java/com/airport/flightscheduler/domain/dto/request/validation/FlightRequestValidator.java
@@ -3,6 +3,7 @@ package com.airport.flightscheduler.domain.dto.request.validation;
 import com.airport.flightscheduler.domain.dto.request.FlightRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.apache.commons.lang3.StringUtils;
 
 public class FlightRequestValidator implements ConstraintValidator<ValidFlightRequest, FlightRequest> {
     @Override
@@ -10,8 +11,7 @@ public class FlightRequestValidator implements ConstraintValidator<ValidFlightRe
         boolean valid = true;
 
         // Origin ≠ Destination
-        if (flightRequest.getOrigin() != null && flightRequest.getDestination() != null &&
-                flightRequest.getOrigin().equalsIgnoreCase(flightRequest.getDestination())) {
+        if (StringUtils.equalsAnyIgnoreCase(flightRequest.getOrigin(), flightRequest.getDestination())) {
             constraintValidatorContext.disableDefaultConstraintViolation();
             constraintValidatorContext.buildConstraintViolationWithTemplate("Origin and Destination must not be same")
                     .addConstraintViolation();
@@ -19,25 +19,20 @@ public class FlightRequestValidator implements ConstraintValidator<ValidFlightRe
         }
 
         //  Departure time ≠ Arrival time
-        if (flightRequest.getDepartureTime() != null && flightRequest.getArrivalTime() != null
-                && flightRequest.getDepartureTime().isEqual(flightRequest.getArrivalTime())) {
+        if (flightRequest.getDepartureTime().isEqual(flightRequest.getArrivalTime())) {
             constraintValidatorContext.disableDefaultConstraintViolation();
             constraintValidatorContext.buildConstraintViolationWithTemplate("Departure and arrival times must not be the same")
                     .addConstraintViolation();
             valid = false;
-
         }
 
         // Departure time must be before Arrival time
-        if (flightRequest.getDepartureTime() != null && flightRequest.getArrivalTime() != null &&
-                flightRequest.getDepartureTime().isAfter(flightRequest.getArrivalTime())) {
-
+        if ( flightRequest.getDepartureTime().isAfter(flightRequest.getArrivalTime())) {
             constraintValidatorContext.disableDefaultConstraintViolation();
             constraintValidatorContext.buildConstraintViolationWithTemplate("Departure time must be before arrival time")
                     .addConstraintViolation();
             valid = false;
         }
-
         return valid;
     }
 
