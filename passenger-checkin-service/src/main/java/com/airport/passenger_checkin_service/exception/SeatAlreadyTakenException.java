8a4158abb15b7ec710195103c4943ae879867a8d
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-21 21:14:41 +0200

    update SeatAlreadyTakenException to handle a single seat instead of a list.

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/exception/SeatAlreadyTakenException.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/exception/SeatAlreadyTakenException.java
@@ -1,9 +1,7 @@
 package com.airport.passenger_checkin_service.exception;
 
-import java.util.List;
-
 public class SeatAlreadyTakenException extends RuntimeException {
-    public SeatAlreadyTakenException(String flightNumber, List<String> seats) {
-        super("Seat(s) " + seats + " already taken for flight " + flightNumber);
+    public SeatAlreadyTakenException(String flightNumber, String seat) {
+        super("Seat " + seat + " already taken for flight " + flightNumber);
     }
 }
