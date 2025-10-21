208c860bf30ddfc41033b13f01c25e41b87cf7e9
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-21 21:46:47 +0200

    Update CheckInStatus enum: remove NOT_CHECKED_IN, add CANCELLED

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/enums/CheckInStatus.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/domain/enums/CheckInStatus.java
@@ -1,9 +1,9 @@
 package com.airport.passenger_checkin_service.domain.enums;
 
 public enum CheckInStatus {
-    NOT_CHECKED_IN,
     CHECKED_IN,
     BOARDING,
     BOARDED,
-    NO_SHOW
+    NO_SHOW,
+    CANCELLED
 }
