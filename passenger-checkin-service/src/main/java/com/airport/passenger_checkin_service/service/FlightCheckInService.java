d79baf8e42f982160d090ff87575f44440bf0b3f
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-10-21 21:19:20 +0200

    Update FlightCheckInService to support multiple seat updates and remove baggage update method

--- passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/FlightCheckInService.java
+++ passenger-checkin-service/src/main/java/com/airport/passenger_checkin_service/service/FlightCheckInService.java
@@ -1,6 +1,7 @@
 package com.airport.passenger_checkin_service.service;
 
 import com.airport.passenger_checkin_service.domain.dto.request.FlightCheckInRequest;
+import com.airport.passenger_checkin_service.domain.dto.request.UpdateSeatRequest;
 import com.airport.passenger_checkin_service.domain.dto.response.FlightCheckInResponse;
 import org.bson.types.ObjectId;
 
@@ -10,7 +11,6 @@ public interface FlightCheckInService {
     FlightCheckInResponse createCheckIn(FlightCheckInRequest request);
     FlightCheckInResponse getCheckInById(ObjectId checkInId);
     List<FlightCheckInResponse> getCheckInsByFlightNumber(String flightNumber);
-    FlightCheckInResponse updateSeatAssignment(ObjectId checkInId, String seatNumber);
-    FlightCheckInResponse updateBaggageInfo(ObjectId checkInId, int baggageCount);
+    FlightCheckInResponse updateSeatNumbers(ObjectId checkInId, UpdateSeatRequest request);
     void cancelCheckIn(ObjectId checkInId);
 }
