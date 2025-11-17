33cf95bf067644fb8af1cfb2c27202605a46184b
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-11-17 19:35:12 +0100

    feat: add Check-Ins and Baggage modules to navbar and routing

--- airport-management-ui/src/App.tsx
+++ airport-management-ui/src/App.tsx
@@ -1,7 +1,9 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-import HomePage from './components/HomePage';
-import FlightsManager from './components/FlightsManager';
-import PassengersManager from './components/PassengersManager';
+import HomePage from './components/HomePage/HomePage.tsx';
+import FlightsManager from './components/Flights/FlightsManager.tsx';
+import PassengersManager from './components/Passengers/PassengersManager.tsx';
+import BaggageManager from "./components/Baggage/BaggageManager.tsx";
+import CheckInsManager from "./components/CheckIns/CheckInsManager.tsx";
 import './App.css';
 
 function App() {
@@ -14,12 +16,15 @@ function App() {
                         <a href="/">Home</a>
                         <a href="/flights">Flights</a>
                         <a href="/passengers">Passengers</a>
+                        <a href="/checkins">Check-Ins</a>
+                        <a href="/baggage">Baggage</a>
                     </nav>
                 </div>
             </header>
             <main>
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
+
                     <Route path="/flights" element={<FlightsManager/>}/>
                     <Route path="/flights/create" element={<FlightsManager/>}/>
                     <Route path="/flights/:flightId" element={<FlightsManager/>}/>
@@ -29,6 +34,20 @@ function App() {
                     <Route path="/passengers/create" element={<PassengersManager/>}/>
                     <Route path="/passengers/:passengerId" element={<PassengersManager/>}/>
                     <Route path="/passengers/:passengerId/edit" element={<PassengersManager/>}/>
+
+                    <Route path="/checkins" element={<CheckInsManager/>}/>
+                    <Route path="/checkins" element={<CheckInsManager/>}/>
+                    <Route path="/checkins/:id" element={<CheckInsManager/>}/>
+                    <Route path="/checkins/create" element={<CheckInsManager/>}/>
+                    <Route path="/checkins/update-seats" element={<CheckInsManager/>}/>
+                    <Route path="/checkins/cancel" element={<CheckInsManager/>}/>
+                    <Route path="/checkins/by-flight" element={<CheckInsManager/>}/>
+
+                    <Route path="/baggage" element={<BaggageManager/>}/>
+                    <Route path="/baggage/:id" element={<BaggageManager/>}/>
+                    <Route path="baggage/update-weight" element={<BaggageManager/>}/>
+                    <Route path="/baggage/update-status" element={<BaggageManager/>}/>
+
 
                     <Route path="*" element={<Navigate to="/" replace/>}/>
                 </Routes>
